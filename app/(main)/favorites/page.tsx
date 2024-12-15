49cf7a085398610038b082cf48d6251e21321482
Luis Meza <ednilsonluis.11@gmail.com>
2024-12-15 12:04:30 -0500

    Version Final V1

--- app/(main)/favorites/page.tsx
+++ app/(main)/favorites/page.tsx
@@ -2,18 +2,23 @@
 
 import Card_Recipe from "@/components/Card_Recipe";
 import { Loader_Recipes } from "@/components/Loaders";
-import useVerifiedUserRegistered from "@/hooks/useVerifiedUserRegistered";
 import { getFavorites } from "@/services/GetFavorites";
 import { authStore } from "@/stores/AuthStore";
 import { Type_Recipes } from "@/types/types";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const PageFavorites = () => {
   const [recipes, setRecipes] = useState<Type_Recipes[]>([]);
   const [loader, setLoader] = useState(false);
   const { user } = authStore();
+  const router = useRouter();
 
-  useVerifiedUserRegistered()
+  useEffect(() => {
+    if (localStorage.getItem("user") == null) {
+      router.push("/home");
+    }
+  }, [router]);
 
   useEffect(() => {
     const handleRecipes = async () => {
@@ -43,9 +48,13 @@ const PageFavorites = () => {
         {loader ? (
           <section className="grid gap-3 my-5 desktop:grid-cols-2 lg:grid-cols-3 lg:w-[90%] lg:mx-auto lg:gap-6 xl:w-[80%]">
             {recipes.length > 0 ? (
-              recipes.map((recipe) => <Card_Recipe key={recipe.id} {...recipe} />)
+              recipes.map((recipe) => (
+                <Card_Recipe key={recipe.id} {...recipe} />
+              ))
             ) : (
-              <p className="text-3xl text-center col-span-3">You don&apos;t have favorite recipes, add recipes to save them</p>
+              <p className="text-3xl text-center col-span-3">
+                You don&apos;t have favorite recipes, add recipes to save them
+              </p>
             )}
           </section>
         ) : (
